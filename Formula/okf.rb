239ae4a79641db771ada5b5b8c37c164f9390233
Akeem Jenkins <accounts@akeemjenkins.com>
2026-08-04 23:00:42 -0600

    okf 0.3.0

--- Formula/okf.rb
+++ Formula/okf.rb
@@ -1,28 +1,28 @@
 class Okf < Formula
   desc "Go CLI toolkit for the Open Knowledge Format (OKF), agentic-first and JSON-native"
   homepage "https://github.com/okfcli/okf"
-  version "0.2.0"
+  version "0.3.0"
   license "Apache-2.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/okfcli/okf/releases/download/v0.2.0/okf_0.2.0_darwin_arm64.tar.gz"
-      sha256 "1a0dc58574f00fa6f9678b924cee11c3abb45546f2156a155768fe404db3ab4a"
+      url "https://github.com/okfcli/okf/releases/download/v0.3.0/okf_0.3.0_darwin_arm64.tar.gz"
+      sha256 "7ea2f6a3ae4ce189c2ecdec8f3ecd100feccb5a720b84467b5312afb75a4cbfa"
     end
     on_intel do
-      url "https://github.com/okfcli/okf/releases/download/v0.2.0/okf_0.2.0_darwin_amd64.tar.gz"
-      sha256 "a96a8830a3bf9a75ed9ee71e4cc8ced7556fd9040c1cf7e9a775d078b6c3347a"
+      url "https://github.com/okfcli/okf/releases/download/v0.3.0/okf_0.3.0_darwin_amd64.tar.gz"
+      sha256 "c5afcee8dac9362af435e23da4ad9624798da6fccdfaa5bab89c45aa0f01ab43"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/okfcli/okf/releases/download/v0.2.0/okf_0.2.0_linux_arm64.tar.gz"
-      sha256 "8a299b08aa6477c1dee2054a60e58e3b6da1dd8b0d10a5ac66af40b024937a27"
+      url "https://github.com/okfcli/okf/releases/download/v0.3.0/okf_0.3.0_linux_arm64.tar.gz"
+      sha256 "b03a75239136a4f0fc5cd1dd60246254e25cd5b13b84cc683e9308360c6b53a5"
     end
     on_intel do
-      url "https://github.com/okfcli/okf/releases/download/v0.2.0/okf_0.2.0_linux_amd64.tar.gz"
-      sha256 "b48408f9131b167a34196f4bff7118340914015261b6206160955addddbb4620"
+      url "https://github.com/okfcli/okf/releases/download/v0.3.0/okf_0.3.0_linux_amd64.tar.gz"
+      sha256 "43b4eb96974d409cc228326bf7976217391454b7d98f18016b00104cf5777509"
     end
   end
 
